b73abc227e5f96c3d8b87a6b0c3b6f7903df91fb
Tarun222999 <tarun@levitation.co.in>
2025-02-19 18:07:52 +0530

    setup server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Tarun222999/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("welcome to students-api")
+	//load config
+	cfg := config.MustLoad()
+	//logger
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to students api"))
+	})
+	//setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	fmt.Printf("server started at %s", cfg.HTTPServer.Addr)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("failer to start server due to %s", err)
+	}
+
 }
