f8a2d4eb71eba811074de24f9951ae31cfed4826
Tarun222999 <tarun@levitation.co.in>
2025-02-21 20:47:13 +0530

    add request validation

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Tarun222999/students-api/internal/config"
+	student "github.com/Tarun222999/students-api/internal/http/handlers/students"
 )
 
 func main() {
@@ -15,20 +21,35 @@ func main() {
 	//setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 	//setup server
 
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
-	fmt.Printf("server started at %s", cfg.HTTPServer.Addr)
-	err := server.ListenAndServe()
 
-	if err != nil {
-		log.Fatal("failer to start server due to %s", err)
+	slog.Info("server starting ", slog.String("address", cfg.HTTPServer.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatal("failed to start server due to %s", err)
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server ")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to stop server due to ", slog.String("error", err.Error()))
 	}
-
 }
